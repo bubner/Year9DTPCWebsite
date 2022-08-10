dac2e6e8bb8c795748d6a55b518332d7666926be
Lucas Bubner <lkbubner@proton.me>
2022-08-10 17:44:12 +0930

    Added fading animations to page transitions

--- public/animmanager.js
+++ public/animmanager.js
@@ -1,10 +1,38 @@
 // Lucas Bubner, 2022
 
 // Get all the divs on the page that are displayed
-// Script is deferred and therefore the DOM should be ready
 const alldivs = document.querySelectorAll('.common div');
+// Get all the anchor tags for fade-out animations
+const allanchors = document.querySelectorAll('a');
 
-// Run over each div and apply an animation to each of them
+// Run over each div and apply a fade in animation to each of them
 for (let i = 0, j = 1; i < alldivs.length; i++, j += 0.25) {
     alldivs[i].style.animation = 'generate ' + j + 's ease';
+}
+
+// Run over each anchor and apply a fade out event listener to each of them
+for (let i = 0; i < allanchors.length; i++) {
+
+    // Pass through any requests that are to the same webpage
+    if (allanchors[i].pathname === window.location.pathname) {
+        continue;
+    }
+
+    // When a click is received, grab the address the user is going to and hold it
+    allanchors[i].addEventListener('click', (e) => {
+        let outgoing = e.currentTarget;
+
+        // Wait until the fade out animation is done, and then allow the webpage to go through
+        let listener = (e) => {
+            window.location = outgoing.href;
+            // Remove the temporary event listeners we made
+            fader.removeEventListener('animationend', listener);
+        }
+
+        // Stop anchor redirect until the fade out animation is done
+        fader.addEventListener('animationend', listener);
+        e.preventDefault();
+        // Start the fade out animation immediately
+        fader.classList.add('activate');
+    });
 }
